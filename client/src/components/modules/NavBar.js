db9043e2e5bebadec5e66ac36f1338a4da359fe0
Emily Zhang <95945661+do9ggy@users.noreply.github.com>
2022-01-18 16:43:41 -0500

    nav bar icons added

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -8,6 +8,24 @@ const NavBar = (props) => {
   return (
     <nav className="NavBar-container">
       <div className="NavBar-linkContainer NavBar-align">
+        <div>
+          <a href="/">
+            <img src="../images/favicon.png" />
+          </a>
+          &emsp;&emsp;&emsp;&ensp;
+          <a href="/requests/">
+            <img src="../images/request.png" />
+          </a>
+          &emsp;&emsp;&emsp;&ensp;
+          <a href="/explore/">
+            <img src="../images/explore.png" />
+          </a>
+          &emsp;&emsp;&ensp;
+          <a href="/fulfill/">
+            <img src="../images/fulfill.png" />
+          </a>
+          &ensp;
+        </div>
         <Link to="/" className="NavBar-link">
           home
         </Link>
@@ -16,12 +34,15 @@ const NavBar = (props) => {
             account
           </Link>
         )}
+
         <Link to="/requests/" className="NavBar-link">
           requests
         </Link>
+
         <Link to="/explore/" className="NavBar-link">
           explore
         </Link>
+
         <Link to="/fulfill/" className="NavBar-link">
           fulfill
         </Link>
