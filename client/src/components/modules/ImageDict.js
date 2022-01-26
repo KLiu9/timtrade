b21d574dfdaa5bf06bd1bd0744ab8fd9d35352a1
Anna Zhang <azhang17@stuy.edu>
2022-01-25 21:58:07 -0500

    added new item images

--- client/src/components/modules/ImageDict.js
+++ client/src/components/modules/ImageDict.js
@@ -1,18 +1,33 @@
 import battery from "../../../dist/itemimages/battery.png";
 import tape from "../../../dist/itemimages/tape.png";
 import mug from "../../../dist/itemimages/mug.png";
-import chair from "../../../dist/itemimages/chair.png";
+import chair from "../../../dist/itemimages/chair2.png";
 import fridge from "../../../dist/itemimages/fridge.png";
 import hairties from "../../../dist/itemimages/hairties.png";
 import ricecooker from "../../../dist/itemimages/ricecooker.png";
 // import kettle from "../../../dist/itemimages/kettle.png";
-import detergent from "../../../dist/itemimages/detergent.png";
+import detergent from "../../../dist/itemimages/detergent2.png";
 import soap from "../../../dist/itemimages/soap.png";
-import bath from "../../../dist/itemimages/bath.png"
+import bath from "../../../dist/itemimages/bath.png";
 // import toothpaste from "../../../dist/itemimages/toothpaste.png";
 import beaver from "../../../dist/itemimages/favicon2.png";
+import toothpaste from "../../../dist/itemimages/toothpaste.png";
+import kettle from "../../../dist/itemimages/kettle.png";
 
-const imageDict = {"batteries": battery, "tape": tape, "mug": mug, "chair": chair, "fridge": fridge, "hair ties": hairties, 
-"rice cooker": ricecooker, "laundry detergent": detergent, "shampoo": bath, "soap": soap, "beaver": beaver};
+const imageDict = {
+  batteries: battery,
+  tape: tape,
+  mug: mug,
+  chair: chair,
+  fridge: fridge,
+  "hair ties": hairties,
+  "rice cooker": ricecooker,
+  "laundry detergent": detergent,
+  shampoo: bath,
+  soap: soap,
+  beaver: beaver,
+  toothpaste: toothpaste,
+  kettle: kettle,
+};
 
 export default imageDict;
